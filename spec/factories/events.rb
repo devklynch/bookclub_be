e0539e45e3002f71ce5e9798c2148384662219e2
devklynch <devklynch@gmail.com>
2025-03-09 12:10:44 -0600

    Made models and references for schema

---
+++ spec/factories/events.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :event do
+    event_name { "MyString" }
+    event_date { "2025-03-09 11:32:38" }
+    location { "MyString" }
+    book { "MyString" }
+    event_notes { "MyString" }
+  end
+end
