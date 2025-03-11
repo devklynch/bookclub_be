51439dc70d37f3d3d432c0d4fec74167da00e924
devklynch <devklynch@gmail.com>
2025-03-11 10:48:12 -0600

    Added relationships and validations for all models

--- app/models/book_club.rb
+++ app/models/book_club.rb
@@ -1,4 +1,10 @@
 class BookClub < ApplicationRecord
     has_many :members, dependent: :destroy
     has_many :users, through: :members
+
+    has_many :events, dependent: :destroy
+    has_many :polls, dependent: :destroy
+
+    validates :name, presence: true, allow_blank: false
+    validates :description, presence: true, allow_bank: true
 end
