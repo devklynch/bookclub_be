43728ec8b6085f17df7545a2aad88fea30409049
devklynch <devklynch@gmail.com>
2025-03-11 09:51:13 -0600

    Created validations and relationships for User and made tests for User

--- app/models/book_club.rb
+++ app/models/book_club.rb
@@ -1,2 +1,4 @@
 class BookClub < ApplicationRecord
+    has_many :members, dependent: :destroy
+    has_many :users, through: :members
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many(:members) }
+    it { should have_many(:book_clubs).through(:members) }
+
+    # it { should have_many(:attendees) }
+    # it { should have_many(:events).through(:attendees) }
+
+    # it { should have_many(:votes) }
+    # it { should have_many(:options).through(:votes) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+
+    it { should validate_presence_of(:display_name) }
+    
+    it { should validate_presence_of(:password) }
+  end
 end
