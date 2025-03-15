612c4f85459b48ced198bf050f3376c05a62044a
devklynch <devklynch@gmail.com>
2025-03-14 18:47:46 -0600

    Route working can now return the event

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::EventsController < ApplicationController
-    
+
+    def show
+        user = User.find(params[:user_id])
+        event = Event.find(params[:id])
+
+        if user.book_clubs.include?(event.book_club)
+            render json: event
+        else
+            render json: {error: "Test Error"}
+        end
+    end
 end
