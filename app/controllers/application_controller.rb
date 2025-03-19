f172737a7359ddb08b324ea4039d6e9cc2e92552
devklynch <devklynch@gmail.com>
2025-03-19 12:26:43 -0600

    Updated for error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: ErrorSerializer.format_errors([e.message]), status: :unprocessable_entity
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: ErrorSerializer.format_errors([e.message]), status: :not_found
+  end
+
+  def render_error
+    render json: ErrorSerializer.format_errors(["Invalid search request"]), status: :bad_request
+  end
+
 end
