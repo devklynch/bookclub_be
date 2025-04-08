00b13a825ec728d83970351f8c2c652b3e276c9c
devklynch <devklynch@gmail.com>
2025-04-07 20:21:15 -0600

    Added sad path tests

--- spec/requests/api/v1/users/registrations_request_spec.rb
+++ spec/requests/api/v1/users/registrations_request_spec.rb
@@ -30,5 +30,49 @@ RSpec.describe "User Registration", type: :request do
       expect(json[:data][:attributes][:password]).to be_nil
       expect(json[:data][:attributes][:password_confirmation]).to be_nil
     end
+
+    it "returns an error when the email is already in use" do
+      post api_v1_users_path, params: {  
+        user: {
+        email: @user.email,
+        password: "password",
+        password_confirmation: "password",
+        display_name: "Test User"
+      }}
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:errors][0][:message]).to eq("Email has already been taken")
+    end
+
+    it "returns an error when a required field is missing" do
+      post api_v1_users_path, params: {
+        user: {
+          email: "",
+          password: "password",
+          password_confirmation: "password",
+          display_name: "Test User"
+        }}
+    
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:errors][0][:message]).to eq("Email can't be blank")
+    end
+
+    it "returns an error when the password confirmation doesn't match" do
+      post api_v1_users_path, params: {
+        user: {
+        email: "test1234@test.com",
+        password: "password1",
+        password_confirmation: "password",
+        display_name: "Test User"
+      }}
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:errors][0][:message]).to eq("Password confirmation doesn't match Password")
+    end
   end
 end
