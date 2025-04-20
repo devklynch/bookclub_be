063d72d84897c46b8eae064afe2c0c9a466971b4
devklynch <devklynch@gmail.com>
2025-04-20 09:35:43 -0600

    Added pundit for auth checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include Pundit::Authorization
 
   rescue_from ActiveRecord::RecordInvalid do |e|
     render json: ErrorSerializer.format_errors(e.record.errors.full_messages), status: :unprocessable_entity
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: ErrorSerializer.format_errors([e.message]), status: :not_found
+  end
+
+  rescue_from Pundit::NotAuthorizedError do |_exception|
+    render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
   end
 
   def render_error
@@ -27,6 +32,7 @@ class ApplicationController < ActionController::Base
       begin
         payload = JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256').first
         @current_user = User.find(payload['user_id'])
+   
       rescue JWT::DecodeError, ActiveRecord::RecordNotFound
         render json: { error: 'Invalid or expired token' }, status: :unauthorized
       end
