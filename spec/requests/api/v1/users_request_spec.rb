8b01da9d0af28e71810ab5db8bce81d06d150de5
devklynch <devklynch@gmail.com>
2025-04-20 10:11:36 -0600

    Added Happy Path Tests

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "User endpoints", type: :request do
+  before :each do
+    @user1 = create(:user)
+    @user2 = create(:user)
+
+    @book_club1 = create(:book_club)
+    @book_club2 = create(:book_club)
+
+    Member.create!(user: @user1, book_club: @book_club1)
+    Member.create!(user: @user2, book_club: @book_club1)
+
+    @poll1 = create(:poll, poll_question: "Next book?", expiration_date: Date.today + 1.week, book_club_id: @book_club1.id)
+    @poll2 = create(:poll, book_club: @book_club1)
+    @poll3 = create(:poll, book_club: @book_club2)
+
+    @option1 = create(:option, option_text: "Gone Girl", additional_info: "Thriller", poll: @poll1)
+    @option2 = create(:option, option_text: "Sherlock Holmes", additional_info: "Classic", poll: @poll1)
+
+    create(:vote, option: @option1, user: @user1)
+    create(:vote, option: @option1, user: @user2)
+    create(:vote, option: @option2, user: @user1)
+    @token1 = @user1.generate_jwt
+  end
+
+  describe "Show all club data for a user" do
+    it "should show a user's data including book clubs, events, and polls" do
+      get all_club_data_api_v1_user_path(id:@user1.id), headers: { 'Authorization' => "Bearer #{@token1}" }
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      #binding.pry
+      expect(response.status).to eq(200)
+      expect(json[:data][:id]).to eq(@user1.id.to_s)
+      expect(json[:data][:type]).to eq("all_club_data")
+      expect(json[:data][:attributes][:id]).to eq(@user1.id)
+      expect(json[:data][:attributes][:display_name]).to eq(@user1.display_name)
+      expect(json[:data][:attributes][:book_clubs].count).to eq(1)
+      expect(json[:data][:attributes][:book_clubs][0][:id]).to eq(@book_club1.id)
+      expect(json[:data][:attributes][:book_clubs][0][:name]).to eq(@book_club1.name)
+      expect(json[:data][:attributes][:upcoming_events]).to eq([])
+      expect(json[:data][:attributes][:active_polls].count).to eq(1)
+      expect(json[:data][:attributes][:active_polls][0][:id]).to eq(@poll1.id)
+      expect(json[:data][:attributes][:active_polls][0][:question]).to eq(@poll1.poll_question)
+
+    end
+  end
+end
