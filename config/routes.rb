5eaa44e348f190b308fa4788afdb83debcd6bd6c
devklynch <devklynch@gmail.com>
2025-07-08 13:23:45 -0600

    Update to show and create events to be under book_club and not user

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,26 @@ Rails.application.routes.draw do
 
       resources :users do
         get "all_club_data", on: :member
-        resources :events, only: [:show, :create] do
-           resources :attendees, only: [:update]# users/user_id/events/event_id
-        end
-        resources :polls, only: [:show] do# users/user_id/polls/poll_id
-          resources :options, only: [] do # users/user_id/polls/poll_id/
-           resources :votes, only: [:create, :destroy] # users/user_id/polls/poll_id/options/option_id/votes
+
+        # Only keep user-book_club routes if needed for show
+        resources :book_clubs, only: [:show]
+
+        resources :polls, only: [:show] do
+          resources :options, only: [] do
+            resources :votes, only: [:create, :destroy]
           end
         end
-        resources :book_clubs, only: [:show] # users/user_id/book_clubs/book_club_id
+      end
+
+      # Routes for creating and showing events under book clubs
+      resources :book_clubs, only: [] do
+        resources :events, only: [:create, :show] do
+          resources :attendees, only: [:update]
+        end
       end
     end
   end
+
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
