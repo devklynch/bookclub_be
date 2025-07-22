9a8caadd65b2c55f9c3bdbdad33d05368c42c695
devklynch <devklynch@gmail.com>
2025-07-22 09:59:24 -0600

    Updated to allow for creation of polls with options

--- app/controllers/api/v1/polls_controller.rb
+++ app/controllers/api/v1/polls_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PollsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
   def show
@@ -11,4 +12,26 @@ class Api::V1::PollsController < ApplicationController
       render json: ErrorSerializer.format_errors(["You are not authorized to view this poll"]), status: :forbidden
     end
   end
+
+  def create
+    book_club = BookClub.find(params[:book_club_id])
+    poll = book_club.polls.new(poll_params)
+    if poll.save
+      render json: PollSerializer.new(poll, params: { current_user: current_user }), status: :created
+    else
+      render json: { errors: poll.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def poll_params
+    params.require(:poll).permit(
+      :poll_question,
+      :expiration_date,
+      :multiple_votes,
+      options_attributes: [:option_text]
+    )
+  end
+
 end
