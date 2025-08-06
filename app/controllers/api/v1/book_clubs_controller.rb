e2822f11651928fb00ef1d2be73c25682a5c4853
devklynch <devklynch@gmail.com>
2025-08-06 12:01:39 -0600

    Update to create a new club

--- app/controllers/api/v1/book_clubs_controller.rb
+++ app/controllers/api/v1/book_clubs_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::BookClubsController < ApplicationController
       render json: ErrorSerializer.format_errors(["You are not authorized to view this book club"]), status: :forbidden
     end
   end
+  
   def create
     book_club = BookClub.new(book_club_params)
     book_club.users << current_user  # Add creator as member
@@ -18,7 +19,7 @@ class Api::V1::BookClubsController < ApplicationController
     if book_club.save
       render json: BookClubSerializer.new(book_club), status: :created
     else
-      render json: { errors: club.errors.full_messages }, status: :unprocessable_entity
+      render json: ErrorSerializer.format_errors(book_club.errors.full_messages), status: :unprocessable_entity
     end
   end
 
@@ -26,13 +27,13 @@ class Api::V1::BookClubsController < ApplicationController
     book_club = BookClub.find(params[:id])
 
     unless book_club.users.include?(current_user)
-      return render json: { errors: ["Not authorized"] }, status: :forbidden
+      return render json: ErrorSerializer.format_errors(["Not authorized"]), status: :forbidden
     end
 
     if book_club.update(book_club_params)
       render json: BookClubSerializer.new(book_club), status: :ok
     else
-      render json: { errors: book_club.errors.full_messages }, status: :unprocessable_entity
+      render json: ErrorSerializer.format_errors(book_club.errors.full_messages), status: :unprocessable_entity
     end
   end
 
